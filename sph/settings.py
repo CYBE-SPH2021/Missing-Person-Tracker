e9ce6cf61eb06105fcbbab1c4ff17d2df5b03e99
Cris10Lobo <cris10lobo@gmail.com>
2021-03-18 12:58:47 +0530

    "linked static fixed login,logo"

--- sph/settings.py
+++ sph/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "Missing-Person-Tracker/static"),
+    os.path.join(BASE_DIR, "sph/static"),
    
 ]
 
