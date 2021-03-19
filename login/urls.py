acac043c5b069916fed47dfe0a2e96a8561fdd57
BryanRonad <bryanronad@gmail.com>
2021-03-19 19:06:24 +0530

    Accounts setup

--- login/urls.py
+++ login/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
     path('dashboard/', include('dashboard.urls')),
 ]
--- login/views.py
+++ login/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
     return render(request,'login/home.html')
+
+def register(request):
+    return render(request,'login/register.html', context)
     
